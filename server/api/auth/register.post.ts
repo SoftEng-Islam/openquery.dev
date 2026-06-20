e180c947ea2c720265389b8f39b603aea2a6ff29
Islam Ahmed <softeng.islam@gmail.com>
2026-06-20 06:19:36 +0300

    update

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,6 +1,11 @@
+import * as argon2 from "argon2";
+
 export default defineEventHandler(async (event) => {
 	const { username, password } = await readBody(event);
 	if (!(username && password)) {
 		throw createError({ statusCode: 400, message: "Username and Password must be provided in data body." });
 	}
+	// Hashing the password before storing it in the database using argon2
+	const hashedPassword = await argon2.hash("password");
+	console.log("Hashed Password:", hashedPassword);
 });
