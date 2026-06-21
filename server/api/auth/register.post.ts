b746a2c295e5459985bcc25751c9b7c78979da76
Islam Ahmed <softeng.islam@gmail.com>
2026-06-21 04:29:46 +0300

    update

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,4 +1,5 @@
 import * as argon2 from "argon2";
+import { table } from "node:console";
 
 export default defineEventHandler(async (event) => {
 	const { username, password } = await readBody(event);
@@ -8,4 +9,10 @@ export default defineEventHandler(async (event) => {
 	// Hashing the password before storing it in the database using argon2
 	const hashedPassword = await argon2.hash("password");
 	console.log("Hashed Password:", hashedPassword);
+	const db = useDrizzle();
+	const insertResult = db.insert(tables.usersTable).values({
+		username: username,
+		password: hashedPassword,
+	}).returning();
+	return { insertResult };
 });
