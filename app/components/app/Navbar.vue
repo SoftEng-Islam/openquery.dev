d1e6d8494a376db69744eeebba0fe21079c56f05
Islam Ahmed <softeng.islam@gmail.com>
2026-06-26 02:20:08 +0300

    update

--- app/components/app/Navbar.vue
+++ app/components/app/Navbar.vue
@@ -7,12 +7,47 @@
 			<li>
 				<nuxtLink to="/about">About</nuxtLink>
 			</li>
-			<li>
-				<nuxtLink to="/register">Register</nuxtLink>
-			</li>
-			<li>
-				<nuxtLink to="/login">Login</nuxtLink>
-			</li>
+			<div
+				v-if="!user"
+				class="flex gap-x-3 flex-row items-center justify-center"
+			>
+				<li>
+					<nuxtLink to="/register">Register</nuxtLink>
+				</li>
+				<li>
+					<nuxtLink to="/login">Login</nuxtLink>
+				</li>
+			</div>
+			<div
+				v-else
+				class="flex gap-x-3 flex-row items-center justify-center"
+			>
+				{{ user.username }}
+			</div>
 		</ul>
 	</nav>
 </template>
+
+<script lang="ts" setup>
+interface User {
+	id: number;
+	username: string;
+}
+const user = ref<User | null>(null);
+
+onMounted(async () => {
+	const token = useCookie("jwt_token");
+	if (!token) {
+		return;
+	}
+	const result = await $fetch("/api/auth/verifyToken", {
+		method: "POST",
+		body: { token: token.value },
+	});
+	if (!result.success) {
+		return;
+	}
+	console.log(result.user);
+	user.value = result.user;
+});
+</script>
---
+++ server/api/auth/verifyToken.post.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export default defineEventHandler(async (event) => {
+	const { token } = await readBody(event);
+	if (!token) {
+		throw createError({ statusCode: 400, message: "No token was passed" });
+	}
+	const user = jwt.verify(token, process.env.JWT_PRIVATE_KEY!);
+	if (user) {
+		return { success: true, user };
+	}
+	throw createError({ statusCode: 401 });
+});
